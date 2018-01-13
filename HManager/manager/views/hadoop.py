d02f59be97a40baaa8f9a69492a7c4d35cd99457
Int-eresting <shushi_5@126.com>
2018-01-13 16:08:58 +0800

    ss

---
+++ HManager/manager/views/hadoop.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import Http404
+
+def hadoop_manage(request):
+    if request.method=='GET':
+        return render(request,'hadoop.html',locals())
+
+def hdfs_ops(request):
+    op=request.GET.get('op')
+    ret = {'status': 1, 'output':'', 'op':op}
+    if op=='start':
+        ret['status'],ret['output']=subprocess.getstatusoutput('start-dfs.sh')
+    elif op=='stop':
+        ret['status'],ret['output']=subprocess.getstatusoutput('stop-dfs.sh')
+    else:
+        ret['status']=1
+        ret['output']='无效命令'
+    return HttpResponse(json.dumps(ret))
