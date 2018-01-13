dc59844af4bb32ee47a755b985a795dc198d91ca
Int-eresting <shushi_5@126.com>
2018-01-13 17:56:57 +0800

    ss

--- HManager/manager/views/hadoop.py
+++ HManager/manager/views/hadoop.py
@@ -58,7 +58,7 @@ def file_download(request):
         ret['status'],ret['output']=subprocess.getstatusoutput('hadoop fs –get %s %s'%(src, des))
     return HttpResponse(json.dumps(ret))
 
-def stop_id(request):
+def stop_job(request):
     id=request.GET.get('id')
     ret = {'status': 1, 'output':'JobID错误'}
     if id:
